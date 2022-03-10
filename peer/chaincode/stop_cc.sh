20c57543c2fdfdbe026f6ec6290910c298ad5c52
leyle <leyle@leyle.com>
2022-03-10 03:37:34 +0000

    update channel/application/lifecycleendorsement policy

---
+++ peer/chaincode/stop_cc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# start chaincode asset transfer basic
+
+# base path means current peer node work dir(e.g $SOMEPATH/nodevolume/peer0.dev.emali.dev)
+DEBUG_BASE_PATH=$PWD/../
+
+ENV_FILE=$DEBUG_BASE_PATH/env.sh
+PEER_ENV_FILE=$DEBUG_BASE_PATH/peer.env
+CC_ENV_FILE=$DEBUG_BASE_PATH/chaincode/chaincode.env
+CC_LAST_ENV_FILE=$DEBUG_BASE_PATH/chaincode/last.env
+
+. $ENV_FILE
+. $PEER_ENV_FILE
+. $CC_ENV_FILE
+. $CC_LAST_ENV_FILE
+
+echo "try to get cd id..."
+echo $FABRIC_CFG_PATH
+CC_LABEL=$CC_NAME
+CC_PKG_ID=$(peer lifecycle chaincode queryinstalled -O json | jq --arg LABEL $CC_LABEL -r '.installed_chaincodes[] | select(.label==$LABEL).package_id')
+echo "package id is: $CC_PKG_ID"
+if [ -z "$CC_PKG_ID" ]; then
+    echo "get chaincode package id failed"
+    exit 1
+fi
+
+export CC_ID=$CC_PKG_ID
+docker-compose -f cc.yaml up down -v
