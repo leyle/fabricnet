f9ab1d67a6cd8c6fcc660d8b4c565043ce28ed68
leyle <leyle@leyle.com>
2022-04-01 05:28:34 +0000

    update chaincode query.sh

--- peer/chaincode/query.sh
+++ peer/chaincode/query.sh
@@ -10,7 +10,13 @@ echo "HOST: $CC_HOST_NAME"
 echo "PORT: $CC_PORT"
 echo $CC_HOST_NAME:$CC_PORT
 
+echo "query installed chaincode ------------->"
 peer lifecycle chaincode queryinstalled
 
-echo "query committed chaincodes, name: $CC_NAME"
+echo "query approved chaincode -------------->"
+PEER_LIST=" --peerAddresses $PEER_CONTAINER_NAME:$PEER_PORT --tlsRootCertFiles $TLS_CA_FILE"
+peer lifecycle chaincode queryapproved --channelID ${APP_CHANNEL_NAME} --name ${CC_NAME} --tls --cafile ${TLS_CA_FILE} $PEER_LIST
+
+
+echo "query committed chaincodes, name: $CC_NAME, ----------------->"
 peer lifecycle chaincode querycommitted --channelID ${APP_CHANNEL_NAME} --name ${CC_NAME} --tls --cafile ${TLS_CA_FILE}
