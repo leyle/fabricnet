a386430239e58d2d92fc09d09b2315803556e57e
leyle <leyle@leyle.com>
2022-05-31 16:45:32 +0800

    update syncandquery start scripts

--- apps/syncandquery/stop.sh
+++ apps/syncandquery/stop.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-. ./versions
+. ./envs.sh
 
-export TIMEZONE=Asia/Shanghai
-
-# mongodb info
-export MONGODB_VERSION=$VERSION_MONGODB
-export MONGODB_PORT=27017
-export MONGODB_CONTAINER_NAME=mongodb-syncandquery-$MONGODB_PORT
-export MONGODB_ROOT_USER=rootuser
-export MONGODB_ROOT_PASSWD=rootpasswd
-export MONGODB_APP_DATABASE=dev
-
-# sync and query service info
-export SYNC_AND_QUERY_IMAGE_VERSION=$VERSION_SYNC_AND_QUERY
-export SYNC_AND_QUERY_PORT=3001
-export SYNC_AND_QUERY_CONTAINER_NAME=syncandquery-$SYNC_AND_QUERY_PORT
-export SYNC_AND_QUERY_CONFIG_FILE=./apiconfig.yaml
-export SYNC_AND_QUERY_LOG=./logs/event_server.log
-
-export FABRIC_CONNECTION_FILE=./connection.yaml
-export FABRIC_WALLET=./wallet
+# . ./versions
+# 
+# export TIMEZONE=Asia/Shanghai
+# 
+# # mongodb info
+# export MONGODB_VERSION=$VERSION_MONGODB
+# export MONGODB_PORT=27017
+# export MONGODB_CONTAINER_NAME=mongodb-syncandquery-$MONGODB_PORT
+# export MONGODB_ROOT_USER=rootuser
+# export MONGODB_ROOT_PASSWD=rootpasswd
+# export MONGODB_APP_DATABASE=dev
+# 
+# # sync and query service info
+# export SYNC_AND_QUERY_IMAGE_VERSION=$VERSION_SYNC_AND_QUERY
+# export SYNC_AND_QUERY_PORT=3001
+# export SYNC_AND_QUERY_CONTAINER_NAME=syncandquery-$SYNC_AND_QUERY_PORT
+# export SYNC_AND_QUERY_CONFIG_FILE=./apiconfig.yaml
+# export SYNC_AND_QUERY_LOG=./logs/event_server.log
+# 
+# export FABRIC_CONNECTION_FILE=./connection.yaml
+# export FABRIC_WALLET=./wallet
 
 
 if [ ! -f $FABRIC_CONNECTION_FILE ]; then
